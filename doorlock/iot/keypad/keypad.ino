d51a0dcbdbf8d3a8eadce881c4659236e1d059c9
ADMIN <hongquan.contact@gmail.com>
2023-03-01 20:46:31 +0700

    up

--- doorlock/iot/keypad/keypad.ino
+++ doorlock/iot/keypad/keypad.ino
@@ -1,14 +1,25 @@
 #include <Keypad.h>
+#include <Servo.h>  // Thêm thư viện servo motor
 
-const byte ROWS = 4; // số hàng
-const byte COLS = 4; // số cột
+#define ledRedPin 4
+#define ledGreenPin 5
+const byte ROWS = 4;  // số hàng
+const byte COLS = 4;  // số cột
+
+
+
+// Khai báo đối tượng servo
+Servo myServo;
+
+// Khai báo chân của servo motor
+const int servoPin = 9;
 
 // Khai báo các ký tự của keypad 4x4
 char keys[ROWS][COLS] = {
-  {'1', '2', '3', 'A'},
-  {'4', '5', '6', 'B'},
-  {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}
+  { '1', '2', '3', 'A' },
+  { '4', '5', '6', 'B' },
+  { '7', '8', '9', 'C' },
+  { '*', '0', '#', 'D' }
 };
 
 // Khai báo các chân kết nối giữa keypad và Arduino
@@ -19,29 +30,49 @@ byte colPins[COLS] = { 5, 4, 3, 2 };
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 // Khai báo mật khẩu
-const char passcode[4] = {'1', '2', '3', '4'};
+const char passcode[4] = { '1', '2', '3', '4' };
 
 void setup() {
-  Serial.begin(9600); // khởi tạo Serial
-  pinMode(13, OUTPUT); // khởi tạo chân kết nối với LED
+  Serial.begin(9600);   // khởi tạo Serial
+  myServo.attach(servoPin);
 }
 
 void loop() {
-  char key = keypad.getKey(); // đọc giá trị từ keypad
-  
+  enterPassword();
+}
+
+void enterPassword() {
+  char key = keypad.getKey();  // đọc giá trị từ keypad
+
   if (key) {
-    Serial.print(key); // hiển thị giá trị đọc được trên Serial Monitor
-    
+    Serial.print(key);  // hiển thị giá trị đọc được trên Serial Monitor
+
     // Kiểm tra mật khẩu
-    if (key == passcode[0] && keypad.waitForKey() == passcode[1] &&
-        keypad.waitForKey() == passcode[2] && keypad.waitForKey() == passcode[3]) {
-      digitalWrite(13, HIGH); // bật đèn LED
-      Serial.println(" - Unlocked"); // hiển thị thông báo trên Serial Monitor
-      delay(5000); // chờ 5 giây
-      digitalWrite(13, LOW); // tắt đèn LED
-    }
-    else {
-      Serial.println(" - Incorrect passcode"); // hiển thị thông báo trên Serial Monitor
+    if (key == passcode[0] && keypad.waitForKey() == passcode[1] && keypad.waitForKey() == passcode[2] && keypad.waitForKey() == passcode[3]) {
+      unlockDoor();
+      Serial.println(" - Unlocked");  // hiển thị thông báo trên Serial Monitor
+      delay(5000);                    // chờ 5 giây
+      lockDoor();
+    } else {
+      lockDoor();
+      Serial.println(" - Incorrect passcode");  // hiển thị thông báo trên Serial Monitor
     }
   }
+}
+
+void unlockDoor() {
+  digitalWrite(ledRedPin, LOW);
+  digitalWrite(ledGreenPin, HIGH);
+  myServo.write(90);  // Quay servo motor ở góc 90 độ
+  delay(1000);        // Dừng 1 giây
+  myServo.write(0);   // Quay servo motor về góc 0 độ
+}
+
+// Hàm  khóa cửa bằng cách quay servo motor
+void lockDoor() {
+  digitalWrite(ledRedPin, HIGH);
+  digitalWrite(ledGreenPin, LOW);
+  myServo.write(0);   // Quay servo motor ở góc 0 độ
+  delay(1000);        // Dừng 1 giây
+  myServo.write(90);  // Quay servo motor về góc 90 độ
 }
--- doorlock/iot/servo/servo.ino
+++ doorlock/iot/servo/servo.ino
@@ -1,5 +1,7 @@
 #include <Servo.h> // Thư viện servo motor
 
+#define ledRedPin 4
+#define ledGreenPin 5
 Servo myServo; // Tạo đối tượng servo
 
 int switchPin = 2; // Chân của công tắc
@@ -13,17 +15,39 @@ void setup() {
 void loop() {
   if (isSwitchPressed()) { // Kiểm tra xem công tắc đã được nhấn chưa
     unlockDoor(); // Mở khóa cửa
+  }else{
+    lockDoor();
   }
 }
 
 // Hàm kiểm tra xem công tắc đã được nhấn chưa
+// bool isSwitchPressed() {
+//   return digitalRead(switchPin) == LOW;
+// }
+
 bool isSwitchPressed() {
-  return digitalRead(switchPin) == LOW;
+  static bool previousState = HIGH;
+  bool currentState = digitalRead(switchPin);
+  bool isPressed = currentState == LOW && previousState == HIGH;
+  previousState = currentState;
+  return isPressed;
 }
+
 
 // Hàm mở khóa cửa bằng cách quay servo motor
 void unlockDoor() {
-  myServo.write(90); // Quay servo motor ở góc 90 độ
-  delay(1000); // Dừng 1 giây
-  myServo.write(0); // Quay servo motor về góc 0 độ
+  digitalWrite(ledRedPin, LOW);
+  digitalWrite(ledGreenPin, HIGH);
+  myServo.write(90);  // Quay servo motor ở góc 90 độ
+  delay(1000);        // Dừng 1 giây
+  myServo.write(0);   // Quay servo motor về góc 0 độ
+}
+
+// Hàm  khóa cửa bằng cách quay servo motor
+void lockDoor() {
+  digitalWrite(ledRedPin, HIGH);
+  digitalWrite(ledGreenPin, LOW);
+  myServo.write(0);   // Quay servo motor ở góc 0 độ
+  delay(1000);        // Dừng 1 giây
+  myServo.write(90);  // Quay servo motor về góc 90 độ
 }
